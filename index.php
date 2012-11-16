d4ff5e2ac5450961a5fe180e71594a08d1b42854
Chris Worfolk <chris.worfolk@bskyb.com>
2012-11-16 13:09:14 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require 'vendor/autoload.php';
+
+WingCommander::init();
+
+Flight::route('/', function(){
+	Flight::view()->set('someVar', 'Hello, World!');
+	Flight::render('hello', array(), 'body');
+	Flight::render('layout', array('title' => 'Test'));
+});
+
+Flight::start();
