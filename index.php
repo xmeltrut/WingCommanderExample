bd4c01a3d7ca7e70841600152ee4e64f4a2b007a
Chris Worfolk <chris@worfolk.co.uk>
2016-01-24 23:32:28 +0000

    Remove minimum stability

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ require 'vendor/autoload.php';
 WingCommander::init();
 
 Flight::route('/', function(){
-	Flight::view()->set('someVar', 'Hello, World!');
-	Flight::render('hello', array(), 'body');
-	Flight::render('layout', array('title' => 'Test'));
+    Flight::view()->set('someVar', 'Hello, World!');
+    Flight::render('hello', array(), 'body');
+    Flight::render('layout', array('title' => 'Test'));
 });
 
 Flight::start();
